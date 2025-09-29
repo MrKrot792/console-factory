10f0e167a5f665e49b6940a9ad6db71c11036453
Vladislav Korotianets <vkorotanec@gmail.com>
2025-09-29 10:49:13 +0200

    Project now depends on ncurses

--- build.zig
+++ build.zig
@@ -4,14 +4,15 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const exe_mod = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    
     const exe = b.addExecutable(.{
         .name = "console_factory",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-
-            .target = target,
-            .optimize = optimize,
-        }),
+        .root_module = exe_mod,
     });
 
     b.installArtifact(exe);
@@ -24,4 +25,7 @@ pub fn build(b: *std.Build) void {
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
+
+    exe.linkSystemLibrary("ncurses");
+    exe.linkLibC();
 }
